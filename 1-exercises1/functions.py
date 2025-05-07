7f9bbad06b5cbba52c64607516d8da9e4be6da8a
Luis Cervantes <lufemarc@gmail.com>
2025-05-06 21:31:19 -0500

    Update functions.py

--- 1-exercises1/functions.py
+++ 1-exercises1/functions.py
@@ -19,7 +19,7 @@ print(meters_to_kilometers(1500))
 # 3. ✉️ Generador de email empresarial 
 
 def create_email(name, lastname, domain):
-    return f"{name.lower()}.{lastname.lower()}@{domain.lower()}.com"
+    return f"{name.lower()}.{lastname.lower()}@{domain.lower()}"
 
 print(create_email("Lucia", "Gomez", "empresa"))
 
@@ -34,15 +34,23 @@ print(final_price(100))
 # 5. 🔐 Simulador de login  
 
 def validate_login(user, password):
-    return print("Successful login") if user == "admin" and password == "1234" else print("Incorrect user or password")
+    if user == "admin":
+        print("Inicio de sesión exitoso")
+        return True
+    else:
+        print ("Credenciales incorrectas")
+        return False
+
 validate_login("admin", "1234")
 validate_login("user", "wrong")
 
 # 6. 🧑‍💻 Generador de nombre de usuario  
 
 def generate_username(name, birth):
-    return print(name+str())
-generate_username("lucho", 1992)
+    return  f"{name.lower()}{date(birth, 1, 1).strftime("%y")}"
+
+
+print(generate_username("Lucas", 1992))
 
 # 7. ✨ Formateador de nombres  
 
@@ -59,14 +67,24 @@ print(calculate_age(1998))
 
 # 9. 📞 Validación de número telefónico  
 
-# def validate_phone(number):
-#     return True if len(number) == 10 else False
+def validate_phone(number):
+    if not len(str(number)) == 10:
+        return False
+    elif type(number) is str:
+        return False
+    else:
+        return True
 
-# print(validate_phone(31452654))
+    #return True if len(number) == 10 else False
+
+
+print(validate_phone(1234567890))
+print(validate_phone(1234))
+print(validate_phone("abcdefghij"))
 
 # 10. 🧠 Notas de estudiantes  
 
-# def student_average(name, grade1, grade2, grade3):
-#     return f"{name}: average: {sum((grade1, grade2, grade3)/3)}"
+def student_average(name, grade1, grade2, grade3):
+    return print(f"{name}: Promedio = {(grade1+grade2+grade3)/3:.2f}")
 
-# print(student_average("ana", 8, 9, 7))
+print(student_average("Ana", 8, 9, 7))
